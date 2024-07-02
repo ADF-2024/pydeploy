16773b82363199e6a54c9d22863ceb5d87ab8e68
Dan <profile@danielseidel.de>
2024-07-02 20:20:29 +0200

    Add condition to set private key for SFTP connections without password.

--- config.py
+++ config.py
@@ -126,7 +126,7 @@ def add_connection():
         target_config['password'] = encrypted_password
         if encryption_strategy == 'password':
             target_config['salt'] = base64.b64encode(salt).decode()
-    else:
+    elif conn_type == 'sftp' and not use_password:
         target_config['private_key'] = encrypted_private_key
         if encryption_strategy == 'password':
             target_config['salt'] = base64.b64encode(salt).decode()
